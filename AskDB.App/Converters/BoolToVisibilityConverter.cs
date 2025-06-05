4d59db724e3475b714d5f8a1e58c4f2cd0aafc52
PHAN Xuan Quang <phanxuanquang2@gmail.com>
2025-06-05 11:38:19 +0700

    Add value converter classes for visibility and API key validation
    Implemented `BoolToVisibilityConverter` to convert boolean values to `Visibility` states. Added `StringToIsValidApiKey` to validate API keys. Both classes implement `IValueConverter` for data binding in XAML, with `ConvertBack` not implemented for the API key converter.

---
+++ AskDB.App/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace AskDB.App.Converters
+{
+    public partial class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool isVisible)
+            {
+                return isVisible ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            throw new InvalidCastException($"Cannot convert from {value} to Visibility data type");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+
+            throw new InvalidCastException($"Cannot convert from {value} to Boolean data type");
+        }
+    }
+}
---
+++ AskDB.App/Converters/StringToIsValidApiKey.cs
@@ -0,0 +1,24 @@
+﻿using GeminiDotNET;
+using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace AskDB.App.Converters
+{
+    public partial class StringToIsValidApiKey : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is string apiKey)
+            {
+                return Validator.CanBeValidApiKey(apiKey);
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
